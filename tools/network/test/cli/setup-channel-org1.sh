4a9e8bae415b2fb548f4e4c2b9cfb01699edbda4
SeoungKwang Kim <net0310@gmail.com>
2019-01-05 15:43:04 +0900

    test 네트워크 프로비저닝 커밋.

--- tools/network/test/cli/setup-channel-org1.sh
+++ tools/network/test/cli/setup-channel-org1.sh
@@ -2,21 +2,21 @@
 
 echo 'Create Channel..'
 
-peer channel create -o orderer0.example.com:7050 -c imchannel -f ./channel-artifacts/channel.tx \
---tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer0.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
+peer channel create -o orderer.fastcampus.com:7050 -c mychannel -f ./channel-artifacts/channel.tx \
+--tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/fastcampus.com/orderers/orderer.fastcampus.com/msp/tlscacerts/tlsca.fastcampus.com-cert.pem
 
 echo 'Done'
 
 echo 'Update Org1 Anchor Peer..'
 
-peer channel update -o orderer0.example.com:7050 -c imchannel -f ./channel-artifacts/${CORE_PEER_LOCALMSPID}anchors.tx  \
---tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer0.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
+peer channel update -o orderer.fastcampus.com:7050 -c mychannel -f ./channel-artifacts/${CORE_PEER_LOCALMSPID}anchors.tx  \
+--tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/fastcampus.com/orderers/orderer.fastcampus.com/msp/tlscacerts/tlsca.fastcampus.com-cert.pem
 
 echo 'Done'
 
 echo 'Join Channel..'
 
-CORE_PEER_ADDRESS=peer0.org1.example.com:7051 peer channel join -b imchannel.block
-CORE_PEER_ADDRESS=peer1.org1.example.com:7051 peer channel join -b imchannel.block
+CORE_PEER_ADDRESS=peer0.org1.fastcampus.com:7051 peer channel join -b mychannel.block
+CORE_PEER_ADDRESS=peer1.org1.fastcampus.com:7051 peer channel join -b mychannel.block
 
 echo 'Done'
--- tools/network/test/cli/setup-channel-org2.sh
+++ tools/network/test/cli/setup-channel-org2.sh
@@ -2,14 +2,14 @@
 
 echo 'Join Channel..'
 
-CORE_PEER_ADDRESS=peer0.org2.example.com:7051 peer channel join -b imchannel.block
-CORE_PEER_ADDRESS=peer1.org2.example.com:7051 peer channel join -b imchannel.block
+CORE_PEER_ADDRESS=peer0.org2.fastcampus.com:7051 peer channel join -b mychannel.block
+CORE_PEER_ADDRESS=peer1.org2.fastcampus.com:7051 peer channel join -b mychannel.block
 
 echo 'Done'
 
 echo 'Update Org2 Anchor Peer..'
 
-peer channel update -o orderer0.example.com:7050 -c imchannel -f ./channel-artifacts/${CORE_PEER_LOCALMSPID}anchors.tx  \
---tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer0.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
+peer channel update -o orderer.fastcampus.com:7050 -c mychannel -f ./channel-artifacts/${CORE_PEER_LOCALMSPID}anchors.tx  \
+--tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/fastcampus.com/orderers/orderer.fastcampus.com/msp/tlscacerts/tlsca.fastcampus.com-cert.pem
 
 echo 'Done'
